0816d2374f273410256206e9161b795f823148ee
syafa <edgina.wicaksono@mhs.unsoed.ac.id>
2026-07-22 09:02:50 +0700

    feat: ganti favicon dan logo jadi logo Dinkominfo Banyumas

--- resources/views/auth/change-password.blade.php
+++ resources/views/auth/change-password.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ubah Kata Sandi Baru - Agendaris</title>
+    <!-- Favicon / Logo Resmi -->
+    <link rel="icon" type="image/png" href="{{ asset('images/logo-banyumas-crest.png') }}">
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700&display=swap');
